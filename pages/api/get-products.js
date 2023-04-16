0f08a62fa6be24b679db252405bb3a59ce1e37fe
Mjd Thif <mjd.thif@icloud.com>
2023-04-16 21:50:34 +0200

    alal

--- pages/api/get-products.js
+++ pages/api/get-products.js
@@ -1,7 +1,7 @@
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
 const api = new WooCommerceRestApi({
-  url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
+  url: "https://backend.carpromorris.se",
   consumerKey: process.env.WC_CONSUMER_KEY,
   consumerSecret: process.env.WC_CONSUMER_SECRET,
   version: "wc/v3",
--- src/utils/slug.js
+++ src/utils/slug.js
@@ -1,6 +1,6 @@
-import { isEmpty } from 'lodash';
+import { isEmpty } from "lodash";
 
-export const FALLBACK = 'blocking';
+export const FALLBACK = "blocking";
 
 /**
  * Check if it's a custom page uri.
@@ -9,16 +9,10 @@ export const FALLBACK = 'blocking';
  * @param uri
  * @return {boolean}
  */
-export const isCustomPageUri = ( uri ) => {
-	const pagesToExclude = [
-		'/',
-		'/blog/',
-		'/checkout/',
-		'/cart/',
-		'/thank-you/',
-	];
-	
-	return pagesToExclude.includes( uri );
+export const isCustomPageUri = (uri) => {
+  const pagesToExclude = ["/", "/blog/", "/checkout/", "/cart/", "/thank-you/"];
+
+  return pagesToExclude.includes(uri);
 };
 
 /**
@@ -29,25 +23,28 @@ export const isCustomPageUri = ( uri ) => {
  * @param field
  * @return {{notFound: boolean}|*|{redirect: {destination: string, statusCode: number}}}
  */
-export const handleRedirectsAndReturnData = ( defaultProps, data, field = '' ) => {
-	
-	// If no data is available then redirect to 503.
-	if ( isEmpty( data ) ) {
-		return {
-			redirect: {
-				destination: '/503',
-				statusCode: 301,
-			},
-		};
-	}
-	
-	// If data for a given field is not available, redirect to 404.
-	if ( field && isEmpty( data?.[ field ] ) ) {
-		return {
-			// returns the default 404 page with a status code of 404
-			notFound: true,
-		};
-	}
-	
-	return defaultProps;
+export const handleRedirectsAndReturnData = (
+  defaultProps,
+  data,
+  field = ""
+) => {
+  // If no data is available then redirect to 503.
+  if (isEmpty(data)) {
+    return {
+      redirect: {
+        destination: "/503",
+        statusCode: 301,
+      },
+    };
+  }
+
+  // If data for a given field is not available, redirect to 404.
+  if (field && isEmpty(data?.[field])) {
+    return {
+      // returns the default 404 page with a status code of 404
+      notFound: true,
+    };
+  }
+
+  return defaultProps;
 };
