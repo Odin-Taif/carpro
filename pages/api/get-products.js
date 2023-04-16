03eee97ab898796ab7d0275bf004efe5730aff9d
Mjd Thif <mjd.thif@icloud.com>
2023-04-16 21:39:12 +0200

    alal

--- pages/api/get-products.js
+++ pages/api/get-products.js
@@ -1,10 +1,10 @@
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
 const api = new WooCommerceRestApi({
-	url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
-	consumerKey: process.env.WC_CONSUMER_KEY,
-	consumerSecret: process.env.WC_CONSUMER_SECRET,
-	version: "wc/v3"
+  url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
+  consumerKey: process.env.WC_CONSUMER_KEY,
+  consumerSecret: process.env.WC_CONSUMER_SECRET,
+  version: "wc/v3",
 });
 
 /**
@@ -17,29 +17,24 @@ const api = new WooCommerceRestApi({
  * @return {Promise<void>}
  */
 export default async function handler(req, res) {
-	
-	const responseData = {
-		success: false,
-		products: []
-	}
-	
-	const { perPage } = req?.query ?? {};
-	
-	try {
-		const { data } = await api.get(
-			'products',
-			{
-				per_page: perPage || 50
-			}
-		);
-		
-		responseData.success = true;
-		responseData.products = data;
-		
-		res.json( responseData );
-		
-	} catch ( error ) {
-		responseData.error = error.message;
-		res.status( 500 ).json( responseData  );
-	}
+  const responseData = {
+    success: false,
+    products: [],
+  };
+
+  const { perPage } = req?.query ?? {};
+
+  try {
+    const { data } = await api.get("products", {
+      per_page: perPage || 50,
+    });
+
+    responseData.success = true;
+    responseData.products = data;
+
+    res.json(responseData);
+  } catch (error) {
+    responseData.error = error.message;
+    res.status(500).json(responseData);
+  }
 }
