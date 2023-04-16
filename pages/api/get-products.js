5ed12e3983d415ddc22e8bc652b677846c39f5e3
Mjd Thif <mjd.thif@icloud.com>
2023-04-16 22:03:39 +0200

    alalalalal

--- pages/api/get-products.js
+++ pages/api/get-products.js
@@ -1,7 +1,7 @@
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
 const api = new WooCommerceRestApi({
-  url: "https://backend.carpromorris.se",
+  url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
   consumerKey: process.env.WC_CONSUMER_KEY,
   consumerSecret: process.env.WC_CONSUMER_SECRET,
   version: "wc/v3",
